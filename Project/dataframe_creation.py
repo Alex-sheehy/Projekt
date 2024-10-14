6272cb5a8a775c85c46f5c0079287db5bb6d5711
Alex-sheehy <Alexander.sheehy00@gmail.com>
2024-10-14 17:52:20 +0200

    Fixat så att adresser och koordinater nu är med i dataframe

--- Project/dataframe_creation.py
+++ Project/dataframe_creation.py
@@ -1,24 +1,29 @@
-from data_processing import ladda_data, rensa_brukar_data, rensa_medarb_data
+from data_processing import ladda_data, rensa_medarb_data
 import pandas as pd
 import re
 
 def skapa_brukare_df(brukare_df,tidsfönsterna,regex_filters):
     """
-    Skapar en dict med data om brukarna i det givna tidsfönstret
+    Input: brukare_df, Det ska vara datan direkt från "ladda_data" funktionen i data_processing
+    tidsfönstrena, alla olika tidsfönster som tuple av sträng och tidsinterval
+    regex_filters, regex för de olika tidsfönstrena
+
+    Skapar en dataframe med data om brukarna i det givna tidsfönstret
     """
+    brukare_df.rename(columns={'Unnamed: 0': 'Individ'}, inplace=True)
+
     index = 0
     tidsfönster_brukare_data = []
     for tidsfönster in tidsfönsterna:
         regex_filter = regex_filters[index]
-        
         #Tar ut individers krav under detta tidsfönster
         läkemedel = brukare_df[brukare_df["Behöver läkemedel"].str.contains(regex_filter, na=False)]
         insulin = brukare_df[brukare_df["Behöver insulin"].str.contains(regex_filter, na=False)]
         stomi = brukare_df[brukare_df["Har stomi"].str.contains(regex_filter, na=False)]
-        dubbelbemanning_df = brukare_df[brukare_df[tidsfönster].str.contains(r'\*', na=False)]
+        dubbelbemanning_df = brukare_df[brukare_df[tidsfönster[0]].str.contains(r'\*', na=False)]
         
         #Tar ut de individer som ska ha besök i detta tidsfönster
-        individer = brukare_df[brukare_df[tidsfönster].apply(
+        individer = brukare_df[brukare_df[tidsfönster[0]].apply(
             lambda x: isinstance(x, int) or 
             (isinstance(x, str) and bool(re.fullmatch(r"\d+\*\d+", x))))]
 
@@ -41,7 +46,7 @@ def skapa_brukare_df(brukare_df,tidsfönsterna,regex_filters):
                 har_stomi = False
             
             if dubbelbemanning_df["Individ"].str.contains(individ, regex = False).any() == True:
-                dubbel_tid = row[tidsfönster].split('*')
+                dubbel_tid = row[tidsfönster[0]].split('*')
                 #Tar ut den tiden för besöket, hanterar olika format som 2*30 och 30*2
                 if dubbel_tid[0] > dubbel_tid[1]:
                     riktig_tid = dubbel_tid[0]
@@ -73,7 +78,7 @@ def skapa_brukare_df(brukare_df,tidsfönsterna,regex_filters):
                 
                 tidsfönster_brukare_data.append({
                         "Individ": individ,
-                        "Tid": row[tidsfönster],
+                        "Tid": row[tidsfönster[0]],
                         "Tidsfönster": tidsfönster,
                         "Constraints": ",".join(filter(None,
                             ['license' if row.get('Kräver körkort', False) == "Ja" else '',
@@ -94,8 +99,11 @@ def skapa_brukare_df(brukare_df,tidsfönsterna,regex_filters):
 
 def skapa_brukare_dag_df(brukare_df, brukare_tidsfönster_df, regex):
     """
-    Skapar en dict för hela dagen. Där även den lägger in speciella villkor
-    som gäller för det specefika dagen. 
+    Input: brukar_df, brukare delen av data gfrån ladda_data
+    brukare_tidsfönster_df, dataframe från skapa_brukare_df funktionen
+    regex, filter för den dagaen som man vill ha data för.
+
+    Skapar en dataframe för den dagens regex filter som man kaller på den med
     """
 
     tid_regex = r'\b\d{2}\b'
@@ -116,35 +124,35 @@ def skapa_brukare_dag_df(brukare_df, brukare_tidsfönster_df, regex):
 
 
         #Kollar om ett besök redan ska ske på förmiddagen, om inte så lägger vi in ett nytt
-        if row["Individ"] in brukare_tidsfönster_df[brukare_tidsfönster_df["Tidsfönster"]=="Förmiddag"]["Individ"].values:
+        if row["Individ"] in brukare_tidsfönster_df[brukare_tidsfönster_df["Tidsfönster"].apply(lambda x: x[0] == "Förmiddag")]["Individ"].values:
             #Uppdaterar tiden för besöket
             orginal_besökstid = brukare_tidsfönster_df.loc[
-                (brukare_tidsfönster_df["Tidsfönster"] == "Förmiddag") & 
+                (brukare_tidsfönster_df["Tidsfönster"].apply(lambda x: x[0] == "Förmiddag")) & 
                 (brukare_tidsfönster_df["Individ"] == row["Individ"]), "Tid"]
             
             ny_besökstid = orginal_besökstid + dusch_tid
 
             orginal_constraints = brukare_tidsfönster_df.loc[(
-                brukare_tidsfönster_df["Tidsfönster"]=="Förmiddag") & (
+                brukare_tidsfönster_df["Tidsfönster"].apply(lambda x: x[0] =="Förmiddag")) & (
                 brukare_tidsfönster_df["Individ"] == row["Individ"]), "Constraints"].values
-            print(orginal_constraints)
+            
             new_constraints = ",".join(filter(None,[str(orginal_constraints).strip("[]\'"), 
                                     'man' if krav_på_man else '',
                                     'woman' if krav_på_kvinna else '']
                                     )).strip(',')
 
             brukare_tidsfönster_df.loc[(
-                brukare_tidsfönster_df["Tidsfönster"]=="Förmiddag") & (
+                brukare_tidsfönster_df["Tidsfönster"].apply(lambda x: x[0] =="Förmiddag")) & (
                 brukare_tidsfönster_df["Individ"] == row["Individ"]), "Tid"] = ny_besökstid
             
             brukare_tidsfönster_df.loc[(
-                brukare_tidsfönster_df["Tidsfönster"]=="Förmiddag") & (
+                brukare_tidsfönster_df["Tidsfönster"].apply(lambda x: x[0] =="Förmiddag")) & (
                 brukare_tidsfönster_df["Individ"] == row["Individ"]), "Constraints"] = new_constraints
         else:
             nytt_besök = pd.DataFrame([{
                 "Individ": row["Individ"],
                 "Tid": dusch_tid,
-                "Tidsfönster": "Förmiddag",
+                "Tidsfönster": ("Förmiddag","9-11"),
                 "Constraints": ",".join(filter(None,
                             ['license' if row.get('Kräver körkort', False) == "Ja" else '',
                             'smoker' if row.get('Röker', False) == "Ja" else '',
@@ -168,16 +176,16 @@ def skapa_brukare_dag_df(brukare_df, brukare_tidsfönster_df, regex):
             aktiverings_tid = int(aktiverings_tid.group())
               #Kollar om ett besök redan ska ske på förmiddagen, om inte så lägger vi in ett nytt
         
-        if row["Individ"] in brukare_tidsfönster_df[brukare_tidsfönster_df["Tidsfönster"]=="Eftermiddag"]["Individ"].values:
+        if row["Individ"] in brukare_tidsfönster_df[brukare_tidsfönster_df["Tidsfönster"].apply(lambda x: x[0] == "Eftermiddag")]["Individ"].values:
             #Uppdaterar tiden för besöket
             orginal_besökstid = brukare_tidsfönster_df.loc[(
-                brukare_tidsfönster_df["Tidsfönster"]=="Eftermiddag") & (
+                brukare_tidsfönster_df["Tidsfönster"].apply(lambda x: x[0]=="Eftermiddag")) & (
                 brukare_tidsfönster_df["Individ"] == row["Individ"]), "Tid"].value
             
             ny_besökstid = orginal_besökstid + aktiverings_tid
 
             brukare_tidsfönster_df.loc[(
-                brukare_tidsfönster_df["Tidsfönster"]=="Eftermiddag") & (
+                brukare_tidsfönster_df["Tidsfönster"].apply(lambda x: x[0]=="Eftermiddag")) & (
                 brukare_tidsfönster_df["Individ"] == row["Individ"]), "Tid"] = ny_besökstid
 
         else:
@@ -185,7 +193,7 @@ def skapa_brukare_dag_df(brukare_df, brukare_tidsfönster_df, regex):
             nytt_besök = pd.DataFrame([{
                 "Individ": row["Individ"],
                 "Tid": aktiverings_tid,
-                "Tidsfönster": "Eftermiddag",
+                "Tidsfönster": ("Eftermiddag","13-15"),
                 "Constraints": ",".join(filter(None,
                             ['license' if row.get('Kräver körkort', False) == "Ja" else '',
                             'smoker' if row.get('Röker', False) == "Ja" else '',
@@ -198,6 +206,40 @@ def skapa_brukare_dag_df(brukare_df, brukare_tidsfönster_df, regex):
 
     return brukare_tidsfönster_df
 
+def addera_adress_till_df(fil,brukare_tidsfönster_df):   
+    adress_till_koordinater = {}
+    individ_till_adress = {}
+    individ_num = 1
+
+    with open(fil, 'r') as f:
+        for line in f:
+            #Delar upp adresserna i aderss och koordinater
+            parts = line.strip().split(", ")
+            adress = parts[0]
+            adress = adress.split(".")[1].strip(" ")
+            latitude =  float, parts[1].strip("()").split(", ")
+            longitude = float, parts[2].strip("()").split(", ")
+            individ = "Individ "+ str(individ_num)
+            #Skapar en dict för adrss kopplat till koordinater
+            adress_till_koordinater[adress] = {"Latitude": latitude, "Longitude": longitude}
+            individ_till_adress[individ] = adress
+            individ_num += 1
+
+    brukare_tidsfönster_df["Individ"] = brukare_tidsfönster_df["Individ"].str.strip()
+
+    #Nu adderar vi adress och koordinater till dataframe för dagen
+    brukare_tidsfönster_df["Adress"] = brukare_tidsfönster_df["Individ"].map(individ_till_adress)
+    brukare_tidsfönster_df["Latitude"] = brukare_tidsfönster_df["Adress"].map(lambda x: adress_till_koordinater[x]["Latitude"])
+    brukare_tidsfönster_df["Longitude"] = brukare_tidsfönster_df["Adress"].map(lambda x: adress_till_koordinater[x]["Longitude"])
+
+    brukare_tidsfönster_df["Latitude"] = brukare_tidsfönster_df["Latitude"].apply(lambda x: x[1] if isinstance(x, tuple) else x)
+    brukare_tidsfönster_df["Longitude"] = brukare_tidsfönster_df["Longitude"].apply(lambda x: x[1] if isinstance(x, tuple) else x)
+
+    brukare_tidsfönster_df["Latitude"] = brukare_tidsfönster_df["Latitude"].apply(lambda x: x[0] if isinstance(x, list) else x)
+    brukare_tidsfönster_df["Longitude"] = brukare_tidsfönster_df["Longitude"].apply(lambda x: x[0] if isinstance(x, list) else x)
+
+
+    return brukare_tidsfönster_df
 
 def main():
 
@@ -205,26 +247,25 @@ def main():
     data = ladda_data("project/data/Studentuppgift fiktiv planering.xlsx")
     
     # Data rensning
-    brukare_df = rensa_brukar_data(data["brukare"])
     medarbetare_df = rensa_medarb_data(data["medarbetare"])
 
-    tid = ["Morgon", "Förmiddag", "Lunch", "Eftermiddag", "Middag", "Tidig kväll", "Sen kväll"]
+    tidsfönster = [("Morgon","7-9"), ("Förmiddag","9-11"), ("Lunch","11-13"), ("Eftermiddag","13-15"), ("Middag","15-17"), ("Tidig kväll","17-19"), ("Sen kväll","19-21")]
     regex_tid_mönster = [ r'\b[mM]org\b', r'\b[fF]m\b', r'\b[lL]unch\b',  r'\b[eE]m\b', r'\b[mM]iddag\b', 
                          r'\b[tT]idig kväll\b', r'\b[sS]en kväll\b']
-    
 
     #Skapar dicts för alla olika tidsfönster som besök kan ske med data om brukare
-    brukare_tidsfönster_df = skapa_brukare_df(data["brukare"], tid, regex_tid_mönster)
-    
-    #print(brukare_tidsfönster_df[brukare_tidsfönster_df["Tidsfönster"] == "Förmiddag"])
+    brukare_tidsfönster_df = skapa_brukare_df(data["brukare"], tidsfönster, regex_tid_mönster)
 
+    #De olika Regex för de olika dagarana
+    #TODO: detta kan göras mer andvändarvänligt med if statement eller mapping 
     regex_dag_mönster = [r'\b[mM]ån', r'\b[tT]is', r'\b[oO]ns', r'\b[tT]or', r'\b[fF]re']
     
+    #Test med en mån dataframe
     mån_df = skapa_brukare_dag_df(data["brukare"], brukare_tidsfönster_df,r'\b[mM]ån')
     
-    print(mån_df[mån_df["Tidsfönster"] == "Eftermiddag"])
+    mån_df = addera_adress_till_df("Project/data/UppdateradeAddresser.txt", mån_df)
 
-    
+    print(mån_df[mån_df["Tidsfönster"].apply(lambda x: x[0] == "Eftermiddag")])
 
 if __name__ == '__main__':
     main()
